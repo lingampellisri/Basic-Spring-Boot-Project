e5c1144e8876d95f6f875211178298293694b011
Srinivas <lingampellisrinivas3036@gmail.com>
2025-05-12 15:41:53 +0530

    Dog Component is Created

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+
+    public String fun()
+    {
+        return "Something is not equal to Nothing";
+    }
+
+
+
+}
--- src/main/java/com/example/demo/myClass.java
+++ src/main/java/com/example/demo/myClass.java
@@ -1,15 +1,27 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class myClass {
 
+    @Autowired
+    private  Dog dog;
+
+    @GetMapping("/ok")
+    public  String ok()
+    {
+      return dog.fun()+" -Srinivas Lingampelli";
+    }
+
+
     @GetMapping("welcome")
 public String sayHello()
 {
-    return "<h1>Hello Spring Boot Tutorial  Iam Happy for this</h1>";
+    return "<h1>Hello Spring Boot Tutorial  Iam Happy for this</h1>" +
+            "<Input type='text' placeholder='Enter name' />";
 }
 
 }
